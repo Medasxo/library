63affb43d92759701b054c382a4454d2b6828f71
Medas <medas02345@gmail.com>
2022-07-14 15:26:02 +0300

    Modify and style displayLIbrary function, so that book's information can be visible on the page

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function () {
-        return title + " by " + author + ", " + pages + ", " + read;
+        return  title + " by " + author + ", " + pages + ", " + read;
     }
 }
 
@@ -15,13 +15,32 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayLibrary(){
-    console.log(myLibrary.length);
+    const libraryContainer = document.querySelector(".container");
+    const br = document.createElement("br");
     for(let i = 0; i < myLibrary.length; i++){
-        console.log(myLibrary[i].info());
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("book");
+        
+        bookDiv.innerHTML += myLibrary[i].title;
+        bookDiv.insertAdjacentHTML('beforeend', '<span style="font-size: 15px;">(Title) </span>',);
+        bookDiv.innerHTML += myLibrary[i].author;
+        bookDiv.insertAdjacentHTML('beforeend', '<span style="font-size: 15px;">(Author) </span>',);
+        bookDiv.innerHTML += myLibrary[i].pages;
+        bookDiv.insertAdjacentHTML('beforeend', '<span style="font-size: 15px;">(Pages) </span>',);
+        if(myLibrary[i].read === "Yes"){
+            bookDiv.insertAdjacentHTML('beforeend', '<button type="button" class="yesButton">Read</button>');
+        }
+        else{
+            bookDiv.insertAdjacentHTML('beforeend',  '<button type="button" class="noButton">Not Read</button>');
+        }
+
+        bookDiv.appendChild(br);
+        libraryContainer.appendChild(bookDiv);
+        
     }
 }
 
-addBookToLibrary("Harry Potter", "JK", 290, "yes");
-addBookToLibrary("Harry Pot", "JK", 292, "yes");
+addBookToLibrary("Harry Potter", "JK. Rowling", 290, "Yes");
+addBookToLibrary("Harry Pot", "JK", 292, "No");
 
 displayLibrary();
