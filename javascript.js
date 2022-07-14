f5445e07cf69976a9cb0678333156fae7f41c167
Medas <medas02345@gmail.com>
2022-07-14 18:39:21 +0300

    Modify addBookToLibrary: now blank parameters cannot be sent and form values reset after pressing submit

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function () {
-        return  title + " by " + author + ", " + pages + ", " + read;
+        return title + " by " + author + ", " + pages + ", " + read;
     }
 }
 
@@ -16,41 +16,48 @@ function addBookToLibrary() {
     let author = form.elements['author'].value;
     let pages = form.elements['pages'].value;
     let read = form.elements['read'].value;
-    myLibrary[myLibrary.length] = new Book(title, author, pages, read);
-    displayLibrary();
-    form.style.display = 'none';
-    
+    if (title === '' || author === '' || pages === '' || read === '') {
+        return;
+    }
+    else {
+        myLibrary[myLibrary.length] = new Book(title, author, pages, read);
+        displayLibrary();
+        form.style.display = 'none';
+        form.reset();
+    }
+
+
 }
 
-function displayLibrary(){
+function displayLibrary() {
     const libraryContainer = document.querySelector(".container");
-    while(document.querySelector(".book") !== null){
-        
+    while (document.querySelector(".book") !== null) {
+
         const deleteDiv = document.querySelector(".book");
         libraryContainer.removeChild(deleteDiv)
     }
     console.log("a");
     const br = document.createElement("br");
-    for(let i = 0; i < myLibrary.length; i++){
+    for (let i = 0; i < myLibrary.length; i++) {
         const bookDiv = document.createElement("div");
         bookDiv.classList.add("book");
-        
+
         bookDiv.innerHTML += myLibrary[i].title;
         bookDiv.insertAdjacentHTML('beforeend', '<span style="font-size: 15px;">(Title) </span>',);
         bookDiv.innerHTML += myLibrary[i].author;
         bookDiv.insertAdjacentHTML('beforeend', '<span style="font-size: 15px;">(Author) </span>',);
         bookDiv.innerHTML += myLibrary[i].pages;
         bookDiv.insertAdjacentHTML('beforeend', '<span style="font-size: 15px;">(Pages) </span>',);
-        if(myLibrary[i].read === "Yes"){
+        if (myLibrary[i].read === "Yes") {
             bookDiv.insertAdjacentHTML('beforeend', '<button type="button" class="yesButton">Read</button>');
         }
-        else{
-            bookDiv.insertAdjacentHTML('beforeend',  '<button type="button" class="noButton">Not Read</button>');
+        else {
+            bookDiv.insertAdjacentHTML('beforeend', '<button type="button" class="noButton">Not Read</button>');
         }
 
         bookDiv.appendChild(br);
         libraryContainer.appendChild(bookDiv);
-        
+
     }
 }
 
@@ -59,12 +66,12 @@ function displayLibrary(){
 
 
 const btn = document.getElementById('newBook');
-btn.addEventListener('click', () =>{
+btn.addEventListener('click', () => {
     const form = document.getElementById('form');
-    if(form.style.display === 'none'){
+    if (form.style.display === 'none') {
         form.style.display = 'block';
     }
-    else{
+    else {
         form.style.display = 'none';
     }
 })
